3e562316cf82f332c449dc55533e2d2df0853611
Ty Overby <ty@pre-alpha.com>
2014-11-22 22:16:53 -0800

    add docs

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['bchannel'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;T, E&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='bchannel/struct.Sender.html' title='bchannel::Sender'>Sender</a>&lt;T, E&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['bchannel'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, T, E&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a>&lt;T&gt; for <a class='struct' href='bchannel/struct.ReceiverIterator.html' title='bchannel::ReceiverIterator'>ReceiverIterator</a>&lt;'a, T, E&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
